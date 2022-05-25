19de99eb527318a18c10efc9a18c10df79935d9c
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2022-05-25 20:33:56 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Quiz Builder App</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Next.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Details from "./Details";
+import Header from "./Header";
+import Mcq from "./Mcq";
+
+function App() {
+  const [details, setDetails] = useState([]);
+  const [crr_option, setCrr_option] = useState([]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Details
+              details={details}
+              setDetails={setDetails}
+              crr_option={crr_option}
+              setCrr_option={setCrr_option}
+            />
+          </Route>
+          <Route path="/mcq">
+            <Mcq details={details} crr_option={crr_option} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
